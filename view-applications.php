b916626e89bb724143a5b66fa6d16e2701936bf6
abuDujaanah <abudujanaally@gmail.com>
2024-08-12 10:16:53 +0300

    Simplify with null coalescing operator
    You can simplify the ternary operation using the null coalescing operator (`??`) introduced in PHP 7. This operator returns the first operand if it exists and is not `null`; otherwise, it returns the second operand.
    
    Here's how you can simplify your code:
    
    ```php
    <?= $a['applicantsID'] ?? ''; ?>
    ```
    
    This will display the value of `$a['applicantsID']` if it is set and not `null`; otherwise, it will display an empty string.

--- view-applications.php
+++ view-applications.php
@@ -110,7 +110,7 @@ $App = $db->getRows("applicants");
                             
                             <div class="col-6">
                                 
-                                <a href="message-users.php?applicant=<?= isset($a['applicantsID']) ? $a['applicantsID'] : ''; ?>" type="button" class="btn btn-danger w-100">reject</a>
+                                <a href="message-users.php?applicant=<?= $a['applicantsID'] ?? ''; ?>" type="button" class="btn btn-danger w-100">reject</a>
                             </div>
 
 
