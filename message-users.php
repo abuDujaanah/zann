6fa85585ef5e181749268354119d37a904617a85
abuDujaanah <abudujanaally@gmail.com>
2024-08-12 17:12:57 +0300

    Tested the application id. Now let's move on

--- message-users.php
+++ message-users.php
@@ -20,7 +20,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Message Users<?php echo $applicantID; ?></title>
+    <title>Message Users</title>
     <style>
         
         body {
