0c7af501ffaa0e8d629222cc55a0f965a5658037
abuDujaanah <abudujanaally@gmail.com>
2024-08-13 17:29:05 +0300

    Create message-users.php

---
+++ message-users.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Send Message Form</title>
+</head>
+<body>
+    <h1>Send a Message</h1>
+    <form action="send_message.php" method="post">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required><br><br>
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br><br>
+
+        <label for="message">Message:</label><br>
+        <textarea id="message" name="message" rows="4" cols="50" required></textarea><br><br>
+
+        <input type="submit" value="Send Message">
+    </form>
+</body>
+</html>
+<?php
+// Database connection details
+$servername = "localhost";
+$username = "user";
+$password = "password";
+$dbname = "database_name";
+
+// Create connection to the database
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Receive data from the form
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// Prepare the SQL query
+$sql = "INSERT INTO messages (name, email, message) VALUES ('$name', '$email', '$message')";
+
+// Insert the data into the database
+if ($conn->query($sql) === TRUE) {
+    echo "Message sent successfully!";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Close the connection
+$conn->close();
+?>
